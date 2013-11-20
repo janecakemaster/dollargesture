2edcb04e036a362b0d0bb612cafe15353d279ef3
Jane Cakemaster <janecakemaster@gmail.com>
2013-11-19 23:44:14 -0500

    some controls work and canvas refresh

--- app/js/app.js
+++ app/js/app.js
@@ -1,28 +1,108 @@
-$( ".help" ).on( "click", function() {
-  $( ".expanded" ).toggle( "fast");
-});
+$(document).ready(onLoadEvent).delay(1000);
 
-$("body").ready( onLoadEvent );
+var video = document.getElementById("vid");
+var canvas = document.getElementById('myCanvas');
+var duration = video.duration;
+var currTime = video.currentTime;
+var timeChunk = duration / 20;
+if (timeChunk > 15)
+  timeChunk = 15;
+var vidSize = video.width;
+var top = 0;
+var left = 0;
+
+if( video.hasAttribute("controls") ){
+  //removes controls programmatically because of chrome screen resize problem
+  video.removeAttribute("controls")
+}
+
+$(".help").on("click", function() {
+  $(".expanded").toggle("fast");
+});
 
 var _isDown, _points, _r, _g, _rc;
 
 function onLoadEvent() {
+  console.log("loading");
   _points = new Array();
   _r = new DollarRecognizer();
 
-  var canvas = document.getElementById('myCanvas');
+  canvas.width = canvas.width;
 
   _g = canvas.getContext('2d');
-  _g.fillStyle = "rgb(0,0,225)";
-  _g.strokeStyle = "rgb(0,0,225)";
+  _g.fillStyle = "#000";
+  _g.strokeStyle = "#000";
   _g.lineWidth = 3;
-  _g.font = "16px Gentilis";
+  _g.font = "16px Arial";
 
   _rc = getCanvasRect(canvas); // canvas rect on page
-  _g.fillStyle = "rgb(255,255,136)";
+  _g.fillStyle = "#7a9fbf";
   _g.fillRect(0, 0, _rc.width, 20);
 
   _isDown = false;
+}
+
+function videoControl(str) {
+  switch(str) {
+    case "play":
+      video.play();
+      // overlay
+      break;
+    case "pause":
+      video.pause();
+      break;
+    case "decrease screen size":
+      changeScreenSize(false);
+      break;
+    case "increase screen size":
+      changeScreenSize(true);
+      break;
+    case "volume up":
+      video.muted = false;
+      video.pause();
+      break;
+    case "volume down":
+      video.muted = false;
+      video.pause();
+      break;
+    case "rewind":
+      video.pause();
+      break;
+    case "fast forward":
+      video.pause();
+      break;
+    case "speed up":
+      video.pause();
+      break;
+    case "slow down":
+      video.pause();
+      break;
+    case "mute":
+      video.muted = true;
+      break;
+    default:
+      console.log("invalid control");
+  }
+  _rc = getCanvasRect(canvas); // canvas rect on page
+}
+
+function changeScreenSize(sign) {
+  var screenSize = $(document).width()
+  if(sign) {
+    if(vidSize === screenSize)
+      return;
+    else {
+      vidSize += 50;
+    }
+  }
+  else {
+    if(vidSize === 200)
+      return;
+    else {
+      vidSize -= 50;
+    }
+  }
+  video.width = vidSize;
 }
 
 function getCanvasRect(canvas) {
@@ -63,7 +143,7 @@ function mouseDownEvent(x, y) {
 
   _points.length = 1; // clear
   _points[0] = new Point(x, y);
-  drawText("Recording unistroke...");
+  drawText("Draw gesture here");
   _g.fillRect(x - 4, y - 3, 9, 9);
 }
 
@@ -85,7 +165,9 @@ function mouseUpEvent(x, y) {
     _isDown = false;
     if (_points.length >= 10) {
       var result = _r.Recognize(_points, false);
-      drawText("Result: " + result.Name + " (" + round(result.Score,2) + ").");
+      var name = result.Name;
+      drawText(name);
+      videoControl(name);
     }
     else // fewer than 10 points were inputted
       drawText("Too few points made. Please try again.");
@@ -93,10 +175,11 @@ function mouseUpEvent(x, y) {
 }
 
 function drawText(str) {
-  _g.fillStyle = "rgb(255,255,136)";
+  _g.fillStyle = "#7a9fbf";
   _g.fillRect(0, 0, _rc.width, 20);
-  _g.fillStyle = "rgb(0,0,255)";
+  _g.fillStyle = "#000";
   _g.fillText(str, 1, 14);
+  _g.font = "bold";
 }
 
 function drawConnectedPoint(from, to) {
@@ -113,29 +196,3 @@ function round(n, d) {
   return Math.round(n * d) / d
 }
 
-// Unistroke Adding and Clearing
-function onClickAddExisting() {
-  if (_points.length >= 10) {
-    var unistrokes = document.getElementById('unistrokes');
-    var name = unistrokes[unistrokes.selectedIndex].value;
-    var num = _r.AddGesture(name, _points);
-    drawText("\"" + name + "\" added. Number of \"" + name + "\"s defined: " + num + ".");
-  }
-}
-
-function onClickAddCustom() {
-  var name = document.getElementById('custom').value;
-  if (_points.length >= 10 && name.length > 0) {
-    var num = _r.AddGesture(name, _points);
-    drawText("\"" + name + "\" added. Number of \"" + name + "\"s defined: " + num + ".");
-  }
-}
-
-function onClickCustom() {
-  document.getElementById('custom').select();
-}
-
-function onClickDelete() {
-  var num = _r.DeleteUserGestures(); // deletes any user-defined unistrokes
-  alert("All user-defined gestures have been deleted. Only the 1 predefined gesture remains for each of the " + num + " types.");
-}
